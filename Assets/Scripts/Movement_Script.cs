effb629ddbb50d7b8b71078c292e29217110acca
Miquel Nadal <miquel.nadal@enti.cat>
2021-04-12 14:31:48 +0200

    Test Scene/ Movimiento y prototipo de Reescalado

---
+++ Assets/Scripts/Movement_Script.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement_Script : MonoBehaviour
+{
+    public float speed;
+    public float jump;
+    private float speedx;
+    public Rigidbody2D rb;
+    private int counter = 0;
+    public float maxX = 5;
+
+
+    float moveVelocity;
+
+    bool grounded = true;
+
+    void Update()
+    {
+        //Jumping 
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (!grounded)
+            {
+                if (counter < 1)
+                {
+                    rb.AddForce(transform.up * jump, ForceMode2D.Impulse);
+                    counter++;
+                }
+            }
+            else
+            {
+                rb.AddForce(transform.up * jump, ForceMode2D.Impulse);
+                counter = 0;
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        //float movingSpeedR = rb.velocity.x + speed; 
+        //float movingSpeedL = rb.velocity.x - speed; 
+        float delta = Time.fixedDeltaTime;
+
+        //Left Right Movement 
+        if (Input.GetKey(KeyCode.A))
+        {
+            if (rb.velocity.x > -maxX)
+            {
+                rb.AddForce((Vector2.left * speed) , ForceMode2D.Force);
+            }
+        }
+
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            if (rb.velocity.x < maxX)
+            {
+                rb.AddForce((Vector2.right * speed) , ForceMode2D.Force);
+            }
+        }
+
+
+    }
+    //Check if Grounded 
+    void OnCollisionEnter2D()
+    {
+        grounded = true;
+    }
+    void OnCollisionExit2D()
+    {
+        grounded = false;
+    }
+
+
+}
---
+++ Assets/Scripts/Scale_Script.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scale_Script : MonoBehaviour
+{
+    public float scaleRate = 1.0f;
+    public float maxScale = 20.0f;
+    public float minScale = 1.0f;
+    public bool freeze = false;
+ 
+ public void ApplyScaleRate()
+    {
+        transform.localScale += Vector3.one * scaleRate;
+    }
+
+    public void mainLoop()
+    {
+        //if we exceed the defined range then correct the sign of scaleRate.
+        if (transform.localScale.x < minScale)
+        {
+            scaleRate = Mathf.Abs(scaleRate);
+        }
+        else if (transform.localScale.x > maxScale)
+        {
+            scaleRate = -Mathf.Abs(scaleRate);
+        }
+        ApplyScaleRate();
+    }
+
+    void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            if (freeze)
+            {
+                freeze = false;
+            }
+            else if (!freeze)
+            {
+                freeze = true;
+            }
+        }
+
+        if (freeze)
+        {
+            mainLoop();
+        }
+    }
+}
