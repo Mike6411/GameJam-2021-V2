9a7c49d6b50693ce3f604f4630b232547f0617d1
Sara <sara.perez@enti.cat>
2021-04-12 19:14:57 +0200

    Menus
    Main menu, final, game over

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+
+    public void StartGame() { 
+    
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    
+    public void CloseGame() {
+        Application.Quit();
+        Debug.Log("Exit");
+    
+    }
+}
