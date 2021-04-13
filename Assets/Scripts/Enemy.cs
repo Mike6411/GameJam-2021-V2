d06943cc8e08e4a9f89e830855fec5856826f0af
Eric.H24 <eric.hernandez@enti.cat>
2021-04-13 22:45:48 +0200

    RETOQUES LVL 1

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+  public float speed;
+  float actualspeed;
+
+  
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        actualspeed=0f;
+        actualspeed=speed;
+        transform.Translate(speed*Time.deltaTime,0,0);
+
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+       if(other.tag=="limitePatrulla"){
+           Debug.Log("chocando");
+           if(transform.rotation.y==0){
+               transform.eulerAngles = new Vector3 (0,180,0);
+
+           }else{
+               transform.eulerAngles = new Vector3 (0,0,0);
+           }
+       } 
+    
+    }
+
+
+}
