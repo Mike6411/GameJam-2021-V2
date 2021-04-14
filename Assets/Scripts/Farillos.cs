3ce122e1bc2868c1e7b1595334751c60e2ad51cd
Miquel Nadal <miquel.nadal@enti.cat>
2021-04-14 10:05:44 +0200

    Script de los farillos

---
+++ Assets/Scripts/Farillos.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class Farillos : MonoBehaviour
+{
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "tapa")
+        {
+            GetComponent<Light2D>().enabled = false;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "tapa")
+        {
+            GetComponent<Light2D>().enabled = true;
+        }
+    }
+}
