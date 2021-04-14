48c6ae79cfd7692c48dcd039853c1e3c3fbb1fc1
Eric.H24 <eric.hernandez@enti.cat>
2021-04-14 03:04:26 +0200

    Sonidos y animaciones

---
+++ Assets/Scripts/SFXenemy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXenemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+     void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag=="Player"){
+         
+        GetComponent<AudioSource>().Play();
+        }
+        
+    }
+
+     void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.tag=="Player"){
+         
+        GetComponent<AudioSource>().Stop();
+        }
+        
+    }
+}
---
+++ Assets/Scripts/Stoptv.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stoptv : MonoBehaviour
+{
+    public GameObject estatica; 
+     void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag=="Player"){
+            Debug.Log("hola otra vez");
+         
+       estatica.SetActive(false);
+        }
+        
+    }
+
+     void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.tag=="Player"){
+         
+        GetComponent<AudioSource>().Stop();
+        }
+        
+    }
+}
